a3e201e4eb87368d72024a294d1b7698e6e98a19
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-15 12:40:54 -0500

    printing messages in two different lines

---
+++ gettingStarted/printingTextNewLines/activity/activitiPrint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+he goal is to print a list of skills you have already mastered to the screen.
+
+The list of skills is the following:
+
+I already know how to:
+- Print text to the screen.
+- Start a new line.
+- Fix errors.
+*/
+
+int main(void){
+	printf("I already know how to:\n");
+	printf("- Print text to the screen.\n");
+	printf("- Start a new line.\n");
+	printf("- Fix errors.");
+	return 0;
+}
---
+++ gettingStarted/printingTextNewLines/activity/build/CMakeFiles/activitiPrint.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/activitiPrint.dir/activitiPrint.c.o"
+  "activitiPrint.pdb"
+  "activitiPrint"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/activitiPrint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
