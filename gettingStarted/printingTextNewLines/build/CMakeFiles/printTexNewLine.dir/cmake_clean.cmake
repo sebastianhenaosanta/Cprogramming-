a670c3ecaada1e42984e5e4b3f4d81d1ddcfa098
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-15 12:30:45 -0500

    printing messages in two different lines

---
+++ gettingStarted/printingTextNewLines/build/CMakeFiles/printTexNewLine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/printTexNewLine.dir/printTexNewLine.c.o"
+  "printTexNewLine.pdb"
+  "printTexNewLine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/printTexNewLine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gettingStarted/printingTextNewLines/printTexNewLine.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/*
+This program prints two diffent messages into the console using the
+special character \n 
+*/
+
+int main(void){
+	printf("Hello World\n");
+	printf("My name is sebastian\n");
+	return 0;
+}
