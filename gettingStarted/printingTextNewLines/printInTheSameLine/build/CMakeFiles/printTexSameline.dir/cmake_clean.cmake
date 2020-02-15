80d9e32877a6a25b3b3cde69977bd6864fb1cc3b
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-15 12:53:44 -0500

    Print multiple lines with one printf statement

---
+++ gettingStarted/printingTextNewLines/printInTheSameLine/build/CMakeFiles/printTexSameline.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/printTexSameline.dir/printTexSameline.c.o"
+  "printTexSameline.pdb"
+  "printTexSameline"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/printTexSameline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gettingStarted/printingTextNewLines/printInTheSameLine/printTexSameline.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+/*
+This program prints two diffent messages into the console using the
+special character \n int the same line
+*/
+
+int main(void){
+	printf("blah\nblah");		
+	return 0;
+}
