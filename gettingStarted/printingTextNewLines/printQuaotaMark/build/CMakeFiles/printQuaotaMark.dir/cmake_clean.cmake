117abd3ba11343c393abc99ebec052c5b4412b97
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-16 13:12:26 -0500

    Print quotation mark and escape special characters

---
+++ gettingStarted/printingTextNewLines/printQuaotaMark/build/CMakeFiles/printQuaotaMark.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/printQuaotaMark.dir/printQuaotaMark.c.o"
+  "printQuaotaMark.pdb"
+  "printQuaotaMark"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/printQuaotaMark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gettingStarted/printingTextNewLines/printQuaotaMark/printQuaotaMark.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/*
+This program prints Quotation marks and special characters\n
+using backslach always after the symbol \"somthing\"
+*/
+
+int main(void){
+	printf("Hello World\n");
+	printf("Have fun with \"this\" \n");
+	return 0;
+}
+
+
