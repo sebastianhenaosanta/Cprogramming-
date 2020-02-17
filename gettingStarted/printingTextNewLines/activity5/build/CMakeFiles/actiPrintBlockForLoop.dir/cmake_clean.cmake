361e7759e3b5ad17e51ce45c58d6dba765512d89
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-17 05:40:47 -0500

    Repeating a block of instructions with a for loop

---
+++ gettingStarted/printingTextNewLines/activity5/build/CMakeFiles/actiPrintBlockForLoop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/actiPrintBlockForLoop.dir/actiPrintBlockForLoop.c.o"
+  "actiPrintBlockForLoop.pdb"
+  "actiPrintBlockForLoop"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/actiPrintBlockForLoop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
