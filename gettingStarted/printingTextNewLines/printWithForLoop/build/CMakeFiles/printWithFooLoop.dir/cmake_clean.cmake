e38384995c15af5b5a21640ca9890c909337aa5f
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-17 05:43:19 -0500

    printing text using a for loop

---
+++ gettingStarted/printingTextNewLines/printWithForLoop/build/CMakeFiles/printWithFooLoop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/printWithFooLoop.dir/printWithFooLoop.c.o"
+  "printWithFooLoop.pdb"
+  "printWithFooLoop"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/printWithFooLoop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gettingStarted/printingTextNewLines/printWithForLoop/printWithFooLoop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/*
+
+Sometimes you need to print out several times a message, one to do it is 
+using a loop.
+
+This programm shows you how to implement a for loop structure to print several times a message.
+*/
+
+int main(void){
+	
+	int i = 0; //Declaring and initialazing a variable
+	//start;stop;steping
+	for(i = 0; i < 100; i++){
+		printf("Hello World for\n");
+	}
+	
+	return 0;
+}
