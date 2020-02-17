9640cf5568e53044e870bf2927e715843b7c1cf5
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-17 05:36:46 -0500

    Printing quotation marks, escaping special characters

---
+++ gettingStarted/printingTextNewLines/activity3/build/CMakeFiles/actiSpecialChact.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/actiSpecialChact.dir/actiSpecialChact.c.o"
+  "actiSpecialChact.pdb"
+  "actiSpecialChact"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/actiSpecialChact.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
