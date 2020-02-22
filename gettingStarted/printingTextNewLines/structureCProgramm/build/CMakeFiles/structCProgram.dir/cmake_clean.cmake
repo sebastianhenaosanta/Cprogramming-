12ab1b7132e0b29830cc26bcbbb54f2e8f383439
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-22 14:12:33 -0500

    structure of a c programm

---
+++ gettingStarted/printingTextNewLines/structureCProgramm/build/CMakeFiles/structCProgram.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/structCProgram.dir/structCProgram.c.o"
+  "structCProgram.pdb"
+  "structCProgram"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/structCProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
