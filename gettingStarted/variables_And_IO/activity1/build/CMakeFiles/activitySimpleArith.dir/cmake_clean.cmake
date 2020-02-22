302ff247e144382bf8512570ec94897e181ec348
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-22 00:18:31 -0500

    simple Arithmetic in c

---
+++ gettingStarted/variables_And_IO/activity1/build/CMakeFiles/activitySimpleArith.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/activitySimpleArith.dir/activitySimpleArith.c.o"
+  "activitySimpleArith.pdb"
+  "activitySimpleArith"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/activitySimpleArith.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
