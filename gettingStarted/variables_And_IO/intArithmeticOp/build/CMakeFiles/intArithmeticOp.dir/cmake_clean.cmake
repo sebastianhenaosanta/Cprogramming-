dcbb69bc7b3b4ff74fae45b4f51b0e11b083ca9c
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-22 00:18:59 -0500

    arithmetic between integers

---
+++ gettingStarted/variables_And_IO/intArithmeticOp/build/CMakeFiles/intArithmeticOp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/intArithmeticOp.dir/intArithmeticOp.c.o"
+  "intArithmeticOp.pdb"
+  "intArithmeticOp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/intArithmeticOp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
