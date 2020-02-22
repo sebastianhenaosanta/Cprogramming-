6664729c9d13ac417e034cea2073229ab191e1d6
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-22 14:07:49 -0500

    scaning data using a for loop

---
+++ gettingStarted/variables_And_IO/scanfLoop/build/CMakeFiles/scanfLoop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scanfLoop.dir/scanfLoop.c.o"
+  "scanfLoop.pdb"
+  "scanfLoop"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/scanfLoop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gettingStarted/variables_And_IO/scanfLoop/scanfLoop.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+This program shows you how to work with scanf inside a for loop
+*/
+
+
+
+int main() {
+    int howMany = 0;
+    int sum = 0;
+    int numberRead = 0;
+    printf("How many items do you want to add?\n");
+    scanf("%d",&howMany);
+    for(int i = 0; i < howMany; i++){
+        scanf("%d",&numberRead);
+        printf("I have read %d from the input terminal\n",numberRead);
+        sum = sum + numberRead;
+        printf("sum equals %d\n",sum);
+    }
+  return 0;
+}
