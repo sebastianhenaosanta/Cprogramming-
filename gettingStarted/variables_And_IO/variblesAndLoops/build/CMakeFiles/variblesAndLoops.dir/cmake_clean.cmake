f8f41631631cec4d79bf4f6d6176e03fc32d3e33
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-22 11:22:40 -0500

    using variables and loops together

---
+++ gettingStarted/variables_And_IO/variblesAndLoops/build/CMakeFiles/variblesAndLoops.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/variblesAndLoops.dir/variblesAndLoops.c.o"
+  "variblesAndLoops.pdb"
+  "variblesAndLoops"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/variblesAndLoops.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gettingStarted/variables_And_IO/variblesAndLoops/variblesAndLoops.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+  This program prints shows you how to variables and loops together
+*/
+
+int main(void) {
+  int i;
+  int numberOfHazelnuts = 0;
+  int distanceTraveled = 0;
+  for (i = 0; i < 9 ; i++) {
+    printf("At %d miles I have %d hazelnuts.\n", distanceTraveled, numberOfHazelnuts);
+    distanceTraveled = distanceTraveled + 1;
+    numberOfHazelnuts = numberOfHazelnuts + 3;
+  }
+  return 0;
+}
