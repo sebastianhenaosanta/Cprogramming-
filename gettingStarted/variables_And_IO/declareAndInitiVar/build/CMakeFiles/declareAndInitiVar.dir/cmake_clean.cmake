d2a00325535949261215b9c77bdd172bdba2e293
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-22 11:21:40 -0500

    declaring and initializing variables in C

---
+++ gettingStarted/variables_And_IO/declareAndInitiVar/build/CMakeFiles/declareAndInitiVar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/declareAndInitiVar.dir/declareAndInitiVar.c.o"
+  "declareAndInitiVar.pdb"
+  "declareAndInitiVar"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/declareAndInitiVar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gettingStarted/variables_And_IO/declareAndInitiVar/declareAndInitiVar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/*
+  This program prints shows you how to declare and initilice variables
+*/
+
+int main(void) {
+  int variable = 2; //variable declaration and initialization all in one step
+
+  return 0;
+}
+
+
+
