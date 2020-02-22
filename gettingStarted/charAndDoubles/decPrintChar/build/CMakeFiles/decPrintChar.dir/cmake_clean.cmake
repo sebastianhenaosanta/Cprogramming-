41f92a8a83f0c9914d855e6ca0a6dc9d7a133d60
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-22 15:13:21 -0500

    declaring and printing char values

---
+++ gettingStarted/charAndDoubles/decPrintChar/build/CMakeFiles/decPrintChar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/decPrintChar.dir/decPrintChar.c.o"
+  "decPrintChar.pdb"
+  "decPrintChar"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/decPrintChar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gettingStarted/charAndDoubles/decPrintChar/decPrintChar.c
@@ -0,0 +1,12 @@
+#include <stdio.h> //header 
+
+
+int main(void){ //main function
+
+	
+	char letter;//DECLARE A CHARACTER VARIABLE
+	letter = 'a';//DEFINE or INITIALIZE or ASSIGN a character value
+	printf("The letter is %c\n",letter);
+	return 0;//end up the function
+
+}
