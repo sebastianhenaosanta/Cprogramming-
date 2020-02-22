44ff4bf3eb51a309aa38f8546cc239060458878f
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-22 14:09:03 -0500

    scanf function

---
+++ gettingStarted/variables_And_IO/inputScanf/build/CMakeFiles/inputScanf.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inputScanf.dir/inputScanf.c.o"
+  "inputScanf.pdb"
+  "inputScanf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/inputScanf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gettingStarted/variables_And_IO/inputScanf/inputScanf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/*
+  This program prints shows you how to scan values from console
+*/
+
+int main(void) {
+  
+	int age;//Declare
+	printf("What's your age ?\n");
+	scanf("%d",&age);
+	printf("You are %d years old\n", age);
+
+  return 0;
+}
+
+
+
