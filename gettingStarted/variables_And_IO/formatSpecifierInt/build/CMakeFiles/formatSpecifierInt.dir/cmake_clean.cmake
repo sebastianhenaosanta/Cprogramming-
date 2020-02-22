0bc14e3061267650159336097378201495279234
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-22 00:16:50 -0500

    format Specifier for integer numbers

---
+++ gettingStarted/variables_And_IO/formatSpecifierInt/build/CMakeFiles/formatSpecifierInt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/formatSpecifierInt.dir/formatSpecifierInt.c.o"
+  "formatSpecifierInt.pdb"
+  "formatSpecifierInt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/formatSpecifierInt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gettingStarted/variables_And_IO/formatSpecifierInt/formatSpecifierInt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/*
+  This program prints shows you how to print messages with integer numbers\n
+  using the special two character (%d) specifier for integers
+
+*/
+
+int main(void) {
+
+
+  printf("If I have %d bills worth %d dollars each then I have %d dollars.", 3, 5, 3 * 5);
+	
+  return 0;
+}
