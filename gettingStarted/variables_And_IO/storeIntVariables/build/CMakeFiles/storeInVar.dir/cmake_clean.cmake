1cab5ce8c9704a7095f68ee04b1a764dc8ebfdb3
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-22 11:22:07 -0500

    storing integer values in variables

---
+++ gettingStarted/variables_And_IO/storeIntVariables/build/CMakeFiles/storeInVar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/storeInVar.dir/storeInVar.c.o"
+  "storeInVar.pdb"
+  "storeInVar"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/storeInVar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gettingStarted/variables_And_IO/storeIntVariables/storeInVar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/*
+  This program prints shows you how to integer values in a variable
+*/
+
+int main(void) {
+
+  //Create a variable to store an integer value
+  int age;
+  //Assigne a value to that variable
+  age = 47;
+  printf("I am %d years old.\n", age);
+  printf("In %d years, I will be %d years old.\n", 8, age + 8);
+  printf("%d years ago, I was %d years old.\n", 11, age - 11);
+  return 0;
+
+
+  return 0;
+}
