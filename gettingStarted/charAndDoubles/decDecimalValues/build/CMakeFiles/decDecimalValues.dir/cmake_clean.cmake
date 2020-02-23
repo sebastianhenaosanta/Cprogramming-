69461118a88b4095a7f6fd1a0d139be5fd495962
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-23 15:45:32 -0500

    reading characters with scanf

---
+++ gettingStarted/charAndDoubles/decDecimalValues/build/CMakeFiles/decDecimalValues.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/decDecimalValues.dir/decDecimalValues.c.o"
+  "decDecimalValues.pdb"
+  "decDecimalValues"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/decDecimalValues.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gettingStarted/charAndDoubles/decDecimalValues/decDecimalValues.c
@@ -0,0 +1,17 @@
+#include <stdio.h> //header 
+/*
+
+This program show you how to declare decimal numbers
+
+*/
+
+int main(void){ //main function
+
+	
+	double height;//DECLARE A CHARACTER VARIABLE
+	height = 1.99;
+	//%.xlf x means the number of decimal values
+	printf("I am %.1lf meters tall.", height);	
+	return 0;//end up the function
+
+}
