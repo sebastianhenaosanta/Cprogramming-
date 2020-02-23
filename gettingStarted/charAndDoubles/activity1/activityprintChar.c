c1f6ee0b6dc52d930005359802d3e16812f55eb9
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-23 15:49:17 -0500

    printing char message

--- gettingStarted/charAndDoubles/activity1/activityprintChar.c
+++ gettingStarted/charAndDoubles/activity1/activityprintChar.c
@@ -1,5 +1,17 @@
 #include <stdio.h> //header 
 
+/*
+Write a C-program that displays the following:
+
+Programming in C
+
+using this printf statement:
+
+printf ("Programming %c%c %c\n", letter1, letter2, letter3);
+
+Warning: do not use a scanf statement in this exercise!
+*/
+
 
 int main(void){ //main function
 
