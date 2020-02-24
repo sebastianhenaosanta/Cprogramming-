77a8458119390bdbf4107e2fa4e4184dbc4f4f14
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-24 18:02:16 -0500

    casting from int to double

---
+++ gettingStarted/charAndDoubles/int2double/build/CMakeFiles/int2double.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/sebastian/Documents/Cprogramming-/gettingStarted/charAndDoubles/int2double/int2double.c" "/home/sebastian/Documents/Cprogramming-/gettingStarted/charAndDoubles/int2double/build/CMakeFiles/int2double.dir/int2double.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ gettingStarted/charAndDoubles/int2double/build/CMakeFiles/int2double.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/int2double.dir/int2double.c.o"
+  "int2double.pdb"
+  "int2double"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/int2double.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
