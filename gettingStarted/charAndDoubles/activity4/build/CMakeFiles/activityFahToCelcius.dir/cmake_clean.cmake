d986924bf3d4ae2ab1f4f5e44b07817e3caa9cba
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-23 21:24:34 -0500

    from fahrenheit to celcius

---
+++ gettingStarted/charAndDoubles/activity4/build/CMakeFiles/activityFahToCelcius.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/activityFahToCelcius.dir/activityFahToCelcius.c.o"
+  "activityFahToCelcius.pdb"
+  "activityFahToCelcius"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/activityFahToCelcius.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
