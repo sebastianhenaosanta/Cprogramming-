b6d9f9dffc28fef6ea19749118aceba69fdf7194
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-23 21:27:37 -0500

    remainderIntDiv/module operator

--- gettingStarted/charAndDoubles/remainderIntDiv/remainderIntDiv.c
+++ gettingStarted/charAndDoubles/remainderIntDiv/remainderIntDiv.c
@@ -8,9 +8,10 @@ This programm show you how to get the remainder of a integer division
 int main(void){
 	
 
-
-
+	int twenties = 166/20;
+	int rest = 166%20;
+	printf("I will pay %d dollars with 20-dollar bills.\n", twenties * 20);
+  printf("I will then pay %d dollars with 1-dollar bills.\n", rest);
+  
 	return 0;
-
-
 }
