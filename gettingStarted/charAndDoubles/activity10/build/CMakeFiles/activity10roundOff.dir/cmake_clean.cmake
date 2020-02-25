9ced112c04b13056eb799b65b3fdf66c117184fe
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-25 18:31:15 -0500

    ceil function to round up

---
+++ gettingStarted/charAndDoubles/activity10/build/CMakeFiles/activity10roundOff.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/activity10roundOff.dir/activity10roundOff.c.o"
+  "activity10roundOff.pdb"
+  "activity10roundOff"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/activity10roundOff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
