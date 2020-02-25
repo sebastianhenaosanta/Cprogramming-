6f8916abff26ab692d80fe45c3e0bd683ad538a5
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-25 18:45:57 -0500

    casting double 2 integers

---
+++ gettingStarted/charAndDoubles/double2Integers/build/CMakeFiles/double2integers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/double2integers.dir/double2integers.c.o"
+  "double2integers.pdb"
+  "double2integers"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/double2integers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gettingStarted/charAndDoubles/double2Integers/build/CMakeFiles/double2integers.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for double2integers.
+# This may be replaced when dependencies are built.
