d155c80bb01a2ff6c92f67552644653268a018dc
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-25 18:33:45 -0500

    casting double to int

---
+++ gettingStarted/charAndDoubles/activity8/activity8Doub2Int.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+/*
+The population of a city has risen sharply over the past few years, thanks to a high birth rate. However, this poses a number of problems, including a housing shortage. The mayor has decided to deal with the problem and would like to estimate the future growth of the population, and he has called you in to help!
+
+Please write a C-program that first reads an integer representing the current population of the city, and that next reads a decimal number for the projected population growth as a percentage (either positive or negative). The program should then display the expected population of the city in a year as a whole number. By convention we will only consider "whole" people. So a population of 31.8 inhabitants will be considered as having 31 inhabitants. 
+
+#example
+
+Input:
+
+123
+7.0
+
+Output:
+131
+
+
+*/
+
+
+
+
+int main(void){
+	
+	int currPopu; 
+	double projPopu;
+	scanf("%d\n%lf", &currPopu, &projPopu);
+	printf("%d\n", (int)(currPopu*(1+(projPopu/100))));
+			
+	return 0;
+}
---
+++ gettingStarted/charAndDoubles/activity8/build/CMakeFiles/activity8Doub2Int.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/activity8Doub2Int.dir/activity8Doub2Int.c.o"
+  "activity8Doub2Int.pdb"
+  "activity8Doub2Int"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/activity8Doub2Int.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
