a90ebb893ea338381ccdb8a14dfa2fb8ffc0cebf
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-03-05 15:33:17 -0500

    print string

---
+++ languageFoundation/stringAndSearchAlgorithms/printStrings/build/CMakeFiles/printStrings.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/printStrings.dir/printStrings.c.o"
+  "printStrings.pdb"
+  "printStrings"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/printStrings.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
