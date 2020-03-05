d1bbde212e3cb909bc161d7f2801b814aa63dfc0
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-03-05 15:22:14 -0500

    temperature monitoring

---
+++ languageFoundation/arrays/activity9/build/CMakeFiles/chemicalExp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chemicalExp.dir/chemicalExp.c.o"
+  "chemicalExp.pdb"
+  "chemicalExp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chemicalExp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
