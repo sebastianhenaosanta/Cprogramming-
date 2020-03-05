077d78c1097f42be5f643f26d95e2f5bc59e4660
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-03-05 14:40:15 -0500

    Controling epidemic

---
+++ languageFoundation/arrays/activity7/build/CMakeFiles/epidemicControl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/epidemicControl.dir/epidemicControl.c.o"
+  "epidemicControl.pdb"
+  "epidemicControl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/epidemicControl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
