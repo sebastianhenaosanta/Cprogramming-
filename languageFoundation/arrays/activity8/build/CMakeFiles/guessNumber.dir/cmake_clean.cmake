f5026f92c3b6c211f9195b565401809ddefdb9bc
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-03-05 15:21:53 -0500

    more or less

---
+++ languageFoundation/arrays/activity8/build/CMakeFiles/guessNumber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/guessNumber.dir/guessNumber.c.o"
+  "guessNumber.pdb"
+  "guessNumber"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/guessNumber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
